39cc959f506fec6489d65f5b07b0b76c489c3a43
Kartik Vegad <kartikvegad1806@gmail.com>
2026-06-13 00:00:00 +0530

    Delete mail_templates directory

--- mail_templates/index.js
+++
@@ -1,3 +0,0 @@
-export { DOT, buildEmail, escapeHtml, paragraph, featureCard } from './shared.js';
-export { newlyRegisteredCompanyEmail } from './newly-registered-company.js';
-export { websiteRedesignOfferEmail } from './website-redesign-offer.js';
